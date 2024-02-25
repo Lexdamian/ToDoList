ced6ebd4a97e6f06bb843a65fefe480f92e0829d
Alexandru Damian <damian.alecsandru@gmail.com>
2024-02-25 10:27:12 +0200

    Added profile view and logout functionality

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -5,18 +5,72 @@
 //  Created by Alexandru Damian on 2/23/24.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewViewModel()
     
+   
+    init() {
+        
+    }
+
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        //Info
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Member since: ")
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        //Signout
+        
+        Button("Sign out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        
+        Spacer()
     }
 }
 
